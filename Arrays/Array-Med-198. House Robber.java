d2909d9517a1cafefa2e4674640e1d22f911f204
sreerohithpeddi <sreerohithpeddi@gmail.com>
2026-07-04 01:33:02 +0530

    added maxHeap approach at 2530 max score at k times

--- Arrays/Array-Med-198. House Robber.java
+++ Arrays/Array-Med-198. House Robber.java
@@ -1,21 +1,29 @@
+/**
+     * Intuition Breakdown (Dynamic Programming State):
+     * * Houses (nums):    [ 2 ]       [ 7 ]       [ 3 ]       [ 8 ]
+     * |           |           |           |
+     * Index (i):         i=0         i=1         i=2         i=3
+     * |           |           |           |
+     * DP Array:         [ 2 ] ----> [ 7 ] ----> [ 7 ] ----> [15 ]
+     * /           /
+     * Decisions:                             /           /
+     * At i=2: Max( Rob(3)+DP(0), Skip/DP(1) ) => Max(3+2, 7)  = 7
+     * At i=3: Max( Rob(8)+DP(1), Skip/DP(2) ) => Max(8+7, 7)  = 15
+     * * Formula: result[i] = Math.max(nums[i] + result[i-2], result[i-1])
+     */
 class Solution {
     public int rob(int[] nums) {
-
         if(nums.length<2){
             return nums[0];
         }
-
         int[] result = new int[nums.length];
-
         result[0] = nums[0];
         result[1] = Math.max(nums[0],nums[1]);
-
         for(int i=2;i<nums.length;i++){
             int stealCurrent = result[i-2];
             int notStealCurrent = result[i-1];
             result[i] = Math.max(stealCurrent+nums[i] , notStealCurrent);
         }
-
         return result[nums.length-1];
     }
 }
--- Arrays/Array-Med-2530. Maximal Score After Applying K Operations.java
+++ Arrays/Array-Med-2530. Maximal Score After Applying K Operations.java
@@ -14,3 +14,26 @@ class Solution {
 }
 
 // look for explaination in the book - it is just attempted need to optimize
+/// Use MaxHeap -- PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b)-> b-a); arrange the values in decreasing order
+/// Use MaxHeap -- PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b)-> a-b);   arrange the values in ascending order
+/// 
+/// using decending order arrangement we can get the max value in the queue & make ciel/3 value out of it and push into queue & do this for k times
+/// 
+/// 
+class Solution {
+    public long maxKelements(int[] nums, int k) {
+        long sum =0;
+        int n = nums.length;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b)-> b-a);
+        for(int num:nums){
+            maxHeap.offer(num); // inserting makes O(nlogn)
+        }
+        while(k>0){
+            int max = maxHeap.poll();
+            sum += max;
+            maxHeap.offer((int) Math.ceil(max/3.0)); // O(klogn)
+            k--;
+        }
+        return sum;
+    }
+}
